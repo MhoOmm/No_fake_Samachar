56a3901666ab469212ca74751211c58c43b5276e
Hamza Patel <patelhamza46@gmail.com>
2026-02-27 12:01:53 +0530

    chore: normalize line endings

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="text-3xl font-bold underline text-center">
+        HAMZA PATEL
+      </p>
+    </>
+  )
+}
+
+export default App
