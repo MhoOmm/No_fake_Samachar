d3fd7f8208bf6e1bebba2f50ef0ccd8b92af0d44
Mho <ommtripathi046@gmail.com>
2026-03-04 22:50:47 +0530

    Chatbot working

--- client/components/ChatbotPage/chatbot.jsx
+++ client/components/ChatbotPage/chatbot.jsx
@@ -7,8 +7,8 @@ const Chatbot = () => {
   const [mode, setMode] = useState("ai");
   const [lastAnalyzedInput, setLastAnalyzedInput] = useState("");
 
-  const BACKEND_URL = "http://localhost:4000";
-  // const BACKEND_URL = "https://no-fake-samacharbackend.onrender.com";
+  // const BACKEND_URL = "http://localhost:4000";
+  const BACKEND_URL = "https://no-fake-samacharbackend.onrender.com";
 
   const handleAnalyze = async () => {
     if (!input.trim() || input === lastAnalyzedInput) return;
