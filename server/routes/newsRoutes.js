56e1fc0f70595f7f2e53e2e73e770aafeff94942
asmit759 <asmitsahu759@gmail.com>
2026-03-05 16:45:10 +0530

    resolved merge conflict

--- server/routes/newsRoutes.js
+++ server/routes/newsRoutes.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express = require("express");
 const { getNews } = require("../controllers/newsController");
 
@@ -7,14 +7,5 @@ const router = express.Router();
 // GET /api/news?country=us&category=technology
 router.get("/", getNews);
 
-=======
-const express = require("express");
-const { getNews } = require("../controllers/newsController");
 
-const router = express.Router();
-
-// GET /api/news?country=us&category=technology
-router.get("/", getNews);
-
->>>>>>> d8a3b7eeba2d01a9973f4345b4d9f155d6cea9e1
 module.exports = router;
